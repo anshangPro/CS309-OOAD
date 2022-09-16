c14aff61e0710183738bad067a1ab913573fc115
ZephyrusZhang <2996362441@qq.com>
2022-09-16 19:59:12 +0800

    feat: change the visibility of the battle grid by clicking on the squares

--- Assets/Scripts/BaseBlock.cs
+++ Assets/Scripts/BaseBlock.cs
@@ -2,30 +2,40 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class BaseBlock : MonoBehaviour
 {
+    public bool IsGridVisible = false;
+    public GameObject Block;
+    
     // Start is called before the first frame update
     void Start()
     {
+        Block = transform.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Block.transform.childCount != 0)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                print("hit:" + hit.collider.gameObject.name);
-            }
+            Block.transform.GetChild(0).gameObject.SetActive(IsGridVisible);
         }
+        // if (Input.GetMouseButton(0))
+        // {
+        //     Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //     RaycastHit hit;
+        //     if (Physics.Raycast(ray, out hit))
+        //     {
+        //         print("hit:" + hit.collider.gameObject.name);
+        //     }
+        // }
     }
 
     private void OnMouseDown()
     {
+        IsGridVisible = !IsGridVisible;
         Debug.Log($"Block {transform.localPosition.ToString()} clicked");
     }
 }
