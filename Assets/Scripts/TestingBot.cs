9d7d83f77ccee792eda31963d183a0dd461a6f2f
Marsy <87608363+MarsyCantStopCoding@users.noreply.github.com>
2022-09-17 22:12:11 +0800

    feat: move the testing bot

---
+++ Assets/Scripts/TestingBot.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class TestingBot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public new Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        Console.WriteLine("Game start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * 2);
+    }
+}
