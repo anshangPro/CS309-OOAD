424bc4b209c3f062f240eace344f9b3c6be03843
anshangPro <65436468+anshangPro@users.noreply.github.com>
2022-09-29 21:18:54 +0800

    fix: using correct namespace

--- Assets/Scripts/Interfaces/Clickable.cs
+++
@@ -1,7 +0,0 @@
-interface IClickable {
-    /// <summary>
-    /// when on click, the mouse controller will call the object's isClicked function
-    /// </summary>
-    /// <returns>bool whether the obj finished its click event</returns>
-    bool IsClicked();
-}
