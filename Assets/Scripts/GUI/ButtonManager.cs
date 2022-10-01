972269461d4402113fdf5d0eebf6df5d46d4e40e
manip_ego <52987267+manipEgo@users.noreply.github.com>
2022-10-01 16:21:47 +0800

    Revert "Merge branch 'main' of https://github.com/anshangPro/CS309-OOAD"
    This reverts commit 355ca101ed11288dbbc9a6e1f0b57bb14ac8280e, reversing
    changes made to 2724cd119f45b04f8c571f530edfcf7ec863c26c.

--- Assets/Scripts/GUI/ButtonManager.cs
+++ Assets/Scripts/GUI/ButtonManager.cs
@@ -11,12 +11,8 @@ namespace GUI
     /// </summary>
     public class ButtonManager : MonoBehaviour
     {
-        private ButtonManager Instance { get; set; }
-
+        private static ButtonManager Instance { get; set; }
         private GameManager _gameManager;
-        private GameObject _menuAttack;
-        private GameObject _menuMove;
-        private GameObject _menuStay;
 
         private void Awake()
         {
@@ -32,44 +28,24 @@ namespace GUI
 
         void Start()
         {
-            _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-            _menuAttack = GameObject.Find("MenuAttack");
-            _menuMove = GameObject.Find("MenuMove");
-            _menuStay = GameObject.Find("MenuStay");
+            _gameManager = GameObject.Find("GameObject").GetComponent<GameManager>();
         }
 
-        /// <summary>
-        /// 根据状态机的状态来决定按钮是否可见
-        /// </summary>
-        public void SetButtonVisible()
+        // Update is called once per frame
+        void FixedUpdate()
         {
             //根据当前的状态展示对应的按钮
             switch (_gameManager.status)
             {
                 case GameStatus.Default:
-                    _menuAttack.SetActive(false);
-                    _menuMove.SetActive(false);
-                    _menuStay.SetActive(false);
                     break;
                 case GameStatus.Menu:
-                    _menuAttack.SetActive(true);
-                    _menuMove.SetActive(true);
-                    _menuStay.SetActive(true);
                     break;
                 case GameStatus.Fight:
-                    _menuAttack.SetActive(false);
-                    _menuMove.SetActive(false);
-                    _menuStay.SetActive(false);
                     break;
                 case GameStatus.Move:
-                    _menuAttack.SetActive(false);
-                    _menuMove.SetActive(false);
-                    _menuStay.SetActive(false);
                     break;
                 case GameStatus.Moving:
-                    _menuAttack.SetActive(false);
-                    _menuMove.SetActive(false);
-                    _menuStay.SetActive(false);
                     break;
                 default:
                     break;
