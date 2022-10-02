03cab3625bc805a750732cefc8ee0a1df70d46bc
anshangPro <65436468+anshangPro@users.noreply.github.com>
2022-10-02 16:14:34 +0800

    fix: support button click and remove the ghost of camera move component

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using GUI;
 using Interfaces;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using Util;
 
 public class MouseController : MonoBehaviour
@@ -14,22 +16,49 @@ public class MouseController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0)){ // press left button of the mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            // press left button of the mouse
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             String[] layers = new[] { "UI", "Units", "Map" };
-            foreach (String layer in layers)
+            PointerEventData eData = new PointerEventData(EventSystem.current);
+            eData.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(eData, results);
+            if (results.Count > 0)
             {
-                RaycastHit hitObj;
-                bool hit = Physics.Raycast(ray, out hitObj,Mathf.Infinity, 1 << LayerMask.NameToLayer(layer));
-                if (hit)
+                foreach (RaycastResult r in results)
                 {
-                    Debug.Log($"Hit entity {hitObj.collider.name} at {hitObj.collider.transform.position} in layer {layer}");
-                    if (hitObj.collider.gameObject.GetComponent<IClickable>() != null)
-                        if (hitObj.collider.gameObject.GetComponent<IClickable>().IsClicked())
+                    if (r.gameObject.GetComponent<IClickable>() != null)
+                    {
+                        Debug.Log(
+                            $"Hit entity {results[0].gameObject.name} at {results[0].gameObject.transform.position} in layer EventSystem");
+                        if (r.gameObject.GetComponent<IClickable>().IsClicked())
                         {
                             _buttonManager.SetButtonVisible();
                             break;
                         }
+                    }
+                }
+            }
+            else
+            {
+                foreach (String layer in layers)
+                {
+                    RaycastHit hitObj;
+                    LayerMask.NameToLayer("TransparentFX");
+                    bool hit = Physics.Raycast(ray, out hitObj, Mathf.Infinity, 1 << LayerMask.NameToLayer(layer));
+                    if (hit)
+                    {
+                        Debug.Log(
+                            $"Hit entity {hitObj.collider.name} at {hitObj.collider.transform.position} in layer {layer}");
+                        if (hitObj.collider.gameObject.GetComponent<IClickable>() != null)
+                            if (hitObj.collider.gameObject.GetComponent<IClickable>().IsClicked())
+                            {
+                                _buttonManager.SetButtonVisible();
+                                break;
+                            }
+                    }
                 }
             }
         }
