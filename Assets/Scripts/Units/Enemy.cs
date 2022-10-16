77d41267572c2953c06340edcd4ea072a148c3f0
manip_ego <52987267+manipEgo@users.noreply.github.com>
2022-10-16 22:01:54 +0800

    Merge branch 'main' of https://github.com/anshangPro/CS309-OOAD

--- Assets/Scripts/Units/Enemy.cs
+++ Assets/Scripts/Units/Enemy.cs
@@ -1,6 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GameData;
+
 namespace Units
 {
     public class Enemy : Unit
     {
+        public override void Attack(Unit target)
+        {
+            base.Attack(target);
+        }
+
+        public Unit ChooseUnitToAttack()
+        {
+            //Dictionary<Unit, double> disMap = new Dictionary<Unit, double>();
+            //GameDataManager.Instance.AllUnits.ForEach(enemy =>
+            //{
+            //    Block block = enemy.onBlock;
+            //    double dis = Math.Sqrt(Math.Pow(onBlock.X - block.X, 2) + Math.Pow(onBlock.Z - block.Z, 2));
+            //    disMap.Add(enemy, dis);
+            //});
+            //
+            //disMap = disMap.OrderBy(elem => elem.Value).ToDictionary(elem => elem.Key, elem => elem.Value);
+            //
+            //return disMap.ElementAt(0).Key;
+            return null;
+        }
     }
 }
