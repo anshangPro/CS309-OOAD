2577853e8da40e9f66ffb54511908ff0a78db440
Marsy <87608363+MarsyCantStopCoding@users.noreply.github.com>
2022-10-20 20:07:32 +0800

    feat: skipAttackButton

--- Assets/Scripts/StateMachine/FightMenu.cs
+++ Assets/Scripts/StateMachine/FightMenu.cs
@@ -1,38 +1,41 @@
-using UnityEngine;
 using GameData;
+using UnityEngine;
 
-public class FightMenu : StateMachineBehaviour
+namespace StateMachine
 {
-    private GameDataManager gameData = GameDataManager.Instance;
-
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    public class FightMenu : StateMachineBehaviour
     {
-        gameData.gameStatus = StateMachine.GameStatus.FightMenu;
-        MapManager.Instance.HighlightUnitAtkRange(gameData.SelectedUnit);
+        private GameDataManager gameData = GameDataManager.Instance;
+
+        // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            gameData.gameStatus = StateMachine.GameStatus.FightMenu;
+            MapManager.Instance.HighlightUnitAtkRange(gameData.SelectedUnit);
+        }
+
+        // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+        //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    
+        //}
+
+        // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+        //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    
+        //}
+
+        // OnStateMove is called right after Animator.OnAnimatorMove()
+        //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    // Implement code that processes and affects root motion
+        //}
+
+        // OnStateIK is called right after Animator.OnAnimatorIK()
+        //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    // Implement code that sets up animation IK (inverse kinematics)
+        //}
     }
-
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
