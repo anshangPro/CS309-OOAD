188be5804477cf6d8f799cca885021ba66d9119a
Marsy <87608363+MarsyCantStopCoding@users.noreply.github.com>
2022-11-10 23:55:43 +0800

    refactor: item implemenation

--- Assets/Scripts/GUI/Backpack/Backpack.cs
+++ Assets/Scripts/GUI/Backpack/Backpack.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Units;
+﻿using System.Collections.Generic;
+using Interfaces;
 using UnityEngine;
 
 namespace GUI.Backpack
@@ -11,6 +10,6 @@ namespace GUI.Backpack
         /// <summary>
         /// 玩家背包，因为储存多个物品，所以是一个集合
         /// </summary>
-        public List<Item> bagList = new List<Item>();
+        public List<ITem> BagList = new List<ITem>();
     }
 }
