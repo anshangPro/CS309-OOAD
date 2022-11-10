e94cd684d6fc2ea4dd0951057b13080dab980643
Marsy <87608363+MarsyCantStopCoding@users.noreply.github.com>
2022-11-10 23:33:14 +0800

    feat: update item

---
+++ Assets/Scripts/GUI/Backpack/Backpack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Units;
+using UnityEngine;
+
+namespace GUI.Backpack
+{
+    [CreateAssetMenu(fileName = "New Backpack", menuName = "New Backpack")]
+    public class Backpack : ScriptableObject
+    {
+        /// <summary>
+        /// 玩家背包，因为储存多个物品，所以是一个集合
+        /// </summary>
+        public List<Item> bagList = new List<Item>();
+    }
+}
---
+++ Assets/Scripts/GUI/Backpack/ItemUI.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GUI.Backpack
+{
+    public class ItemUI : MonoBehaviour
+    {
+        public Image itemImage;
+        public Text itemNum;
+    }
+}
