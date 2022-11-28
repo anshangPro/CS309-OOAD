e1535507d77a79a9d4a6c1047fb553afd6d9b199
manip_ego <52987267+manipEgo@users.noreply.github.com>
2022-11-28 21:08:04 +0800

    fix: shaking y

--- Assets/Scripts/GUI/MainMenu/BackgroundControl.cs
+++ Assets/Scripts/GUI/MainMenu/BackgroundControl.cs
@@ -3,63 +3,55 @@ using UnityEngine;
 public class BackgroundControl : MonoBehaviour
 {
     public float base_speed_x = 10f;
-    public float base_speed_y = 30f;
-    public float above_y = 1080f;
-    public float below_y = -1080f;
+    public float base_speed_y = 10f;
+    public float compensate_speed_y = 100f;
+    public float base_above_y = 1080f;
+    public float base_below_y = -1080f;
 
     public float base_y = 0f;
-    public float stop_threshold = 0.1f;
-    private float init_x;
-    private float init_y;
+    public float target_y = 0f;
+    private RectTransform[] init_rects;
+    private RectTransform[] rects;
 
     // Start is called before the first frame update
     void Start()
     {
-        init_x = GetComponentInChildren<Transform>().transform.position.x;
-        init_y = GetComponentInChildren<Transform>().transform.position.y;
-        base_y = init_y;
+        rects = GetComponentsInChildren<RectTransform>();
+        init_rects = (RectTransform[])rects.Clone();
+        base_y = init_rects[2].position.y;
+        target_y = base_y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Transform[] layer_trans = GetComponentsInChildren<Transform>();
-        RectTransform[] layer_rect = GetComponentsInChildren<RectTransform>();
-        for (int i = 1; i < layer_trans.Length; i++)
+        for (int i = 1; i < rects.Length; i++)
         {
-            layer_trans[i].transform.Translate(Vector3.left * base_speed_x * (i>>1) * Time.deltaTime);
-            if (layer_trans[i].transform.position.x < - layer_rect[i].rect.width / 2f)
-                layer_trans[i].transform.Translate(Vector3.right * (layer_rect[i].rect.width + 1920f));
-        }
+            rects[i].Translate(Vector3.left * base_speed_x * (i>>1) * Time.deltaTime);
+            if (rects[i].position.x < - rects[i].rect.width / 2f)
+                rects[i].Translate(Vector3.right * (rects[i].rect.width + 1920f));
 
-        if (layer_trans[2].transform.position.y - base_y < stop_threshold)
-        {
-            for (int i = 2; i < layer_trans.Length; i++)
-            {
-                layer_trans[i].transform.Translate(Vector3.up * base_speed_y * (i >> 1) * Time.deltaTime);
-            }
-        }
-        else if (layer_trans[2].transform.position.y - base_y > stop_threshold)
-        {
-            for (int i = 2; i < layer_trans.Length; i++)
-            {
-                layer_trans[i].transform.Translate(Vector3.down * base_speed_y * (i >> 1) * Time.deltaTime);
-            }
+            if (rects[i].position.y != target_y)
+                rects[i].position = Vector3.MoveTowards(
+                    rects[i].position,
+                    new Vector3(rects[i].position.x, target_y, rects[i].position.z),
+                    (base_speed_y * (i>>1) + compensate_speed_y) * Time.deltaTime
+                );
         }
     }
 
-    public void HideAbove()
-    {
-        base_y = init_y + above_y;
-    }
+    // public void HideAbove()
+    // {
+    //     base_y = init_y + above_y;
+    // }
 
-    public void HideBelow()
-    {
-        base_y = init_y + below_y;
-    }
+    // public void HideBelow()
+    // {
+    //     base_y = init_y + below_y;
+    // }
 
-    public void ShowAtCenter()
-    {
-        base_y = init_y;
-    }
+    // public void ShowAtCenter()
+    // {
+    //     base_y = init_y;
+    // }
 }
