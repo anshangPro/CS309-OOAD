20c0c9632d3e1df1836fe636adf44e392b7b8207
Marsy <87608363+MarsyCantStopCoding@users.noreply.github.com>
2022-11-29 10:07:02 +0800

    feat: shoot text resources

---
+++ Assets/ShootTextPro/Scripts/ShootTextComponent.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ShootTextComponent : MonoBehaviour
+{
+
+    public Animator animator = null;
+    public CanvasGroup canvasGroup = null;
+    public RectTransform rectTransform = null;
+
+    public List<RectTransform> childTransformGroup = new List<RectTransform>();
+
+    public List<Vector2> sizeDeltaGroup = new List<Vector2>();
+
+    #region From ShootTextInfo
+    public string content;
+    public TextAnimationType animationType;
+    public TextMoveType moveType;
+    public double delayMoveTime;
+    public int size;
+    public Transform cacheTranform;
+    public double initializedVerticalPositionOffset;
+    public double initializedHorizontalPositionOffset;
+    #endregion
+
+    public double fadeCurveTime;
+
+    public double xMoveOffeset;
+    public double yMoveOffeset;
+
+    public bool isMove = false;
+    public void SetInfo(ShootTextInfo shootTextInfo)//执行顺序优于Start
+    {
+        content = shootTextInfo.content;
+        animationType = shootTextInfo.animationType;
+        moveType = shootTextInfo.moveType;
+        delayMoveTime = Time.time + shootTextInfo.delayMoveTime;
+        cacheTranform = shootTextInfo.cacheTranform;
+        initializedHorizontalPositionOffset = shootTextInfo.initializedHorizontalPositionOffset;
+        initializedVerticalPositionOffset = shootTextInfo.initializedVerticalPositionOffset;
+    }
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        rectTransform = GetComponent<RectTransform>();
+        childTransformGroup = transform.GetComponentsInChildren<RectTransform>().ToList();
+        childTransformGroup.Remove(childTransformGroup[0]);
+
+        for (int i = 0; i < childTransformGroup.Count; i++)
+        {
+            sizeDeltaGroup.Add(childTransformGroup[i].sizeDelta);
+        }
+        int state = animationType == TextAnimationType.Normal ? 1 : 2;
+        animator.SetInteger("state", state);
+    }
+
+    public void ChangeScale(double scale)
+    {
+        for (int i = 0; i < childTransformGroup.Count; i++)
+        {
+            Vector2 sizeDelta = sizeDeltaGroup[i];
+            sizeDelta.x = sizeDelta.x * (float)scale;
+            sizeDelta.y = sizeDelta.y * (float)scale;
+            childTransformGroup[i].sizeDelta = sizeDelta;
+        }
+    }
+
+    public void ChangeAlpha(float alpha)
+    {
+        canvasGroup.alpha = alpha;
+    }
+}
+
---
+++ Assets/ShootTextPro/Scripts/ShootTextInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootTextInfo
+{
+    public string content;
+    public TextAnimationType animationType;
+    public TextMoveType moveType;
+    public float delayMoveTime;
+    public int size;
+    public Transform cacheTranform;
+    public float initializedVerticalPositionOffset;
+    public float initializedHorizontalPositionOffset;
+    public float xIncrement;
+    public float yIncrement;
+}
+
